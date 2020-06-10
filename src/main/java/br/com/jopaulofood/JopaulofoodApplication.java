0ca500dce0ad3e941d5e5c1f1e4f301b05b6bf43
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-06-10 02:14:08 -0300

    Criando o projeto JopauloFood e importando as telas do projeto

---
+++ src/main/java/br/com/jopaulofood/JopaulofoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.jopaulofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JopaulofoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JopaulofoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jopaulofood/JopaulofoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jopaulofood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JopaulofoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
